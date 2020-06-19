e273d7f58d598635f615cd489ab195eac67143bf
shizofreniyaq <dentaras7@gmail.com>
2020-06-19 14:36:58 +0300

    d`

--- js.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-
-        $('.panel-heading').click(function () {
-            $(this).toggleClass('in').next().slideToggle();
-            $('.panel-heading').not(this).removeClass('in').next().slideUp();
-        });
-
-    });
